a546ba0bd96c8418721d3e8cba160e40585ec81b
Jonas Labode <labode.jonas@mh-hannover.de>
2021-04-23 14:19:49 +0200

    User can now supply an analysis type, placeholder function for an analysis operation added

--- main.py
+++ main.py
@@ -4,15 +4,34 @@ import sys
 
 def read_graph(file):
     # Read graph from .dot file using pydot and turn it into a networkx graph
-    dot_graph = nx.drawing.nx_pydot.read_dot(dotfile)
+    dot_graph = nx.drawing.nx_pydot.read_dot(file)
 
     return dot_graph
+
+
+def generation_analysis():
+    print("Genana here!")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     dotfile = sys.argv[1]
-    graph = read_graph(dotfile)
-    if len(graph) != 0:
-        print("Graph read")
+    analysis_type = sys.argv[2]
+
+    error = False
+
+    if not dotfile or not analysis_type:
+        # TODO: Explain the needed parameters to the user
+        print("Missing parameters")
+        error = True
+
+    if not error:
+        graph = read_graph(dotfile)
+
+        if int(analysis_type) == 0:
+            generation_analysis()
+        else:
+            print("Analysis type not supported")
+
+
 
