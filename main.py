d38bfd0a18eab2756590e8064e0982e305020e4d
Jonas Labode <labode.jonas@mh-hannover.de>
2021-04-23 14:29:53 +0200

    Graph writer implemented

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import numpy as np
+import pydot
 import sys
 
 
@@ -45,6 +46,50 @@ def new_neighbors(node, known):
     return discovered
 
 
+# TODO: Is this really necessary? There must be something like this in numpy!
+def reshape_array(array, width):
+    size = array.size
+    length = size//width
+
+    array = array.reshape((length, width))
+
+    return array
+
+
+def write_dot(graph_data, edges, color=True, label='Gen'):
+    colors = np.array(["red", "green", "blue", "yellow", "cyan", "magenta"], str)
+
+    filename = "output_graph.dot"
+    output_file = open(filename, "w")
+
+    output_file.write("graph G {\n")
+
+    # Get all nodes, print those out
+    nodes = nx.nodes(graph_data)
+    for i in nodes:
+        output_file.write(str(i) + ";\n")
+
+    edges = reshape_array(edges, 3)
+
+    # Write each edge pair with generation marking
+    for i in edges:
+        if color == bool(True):
+            col = int(i[2]) - 1
+            col_str = " color = " + str(colors[col])
+        else:
+            col_str = ""
+        output_file.write(
+            str(i[0]) + "--" + str(i[1]) + " [ label = \"" + label + " " + str(i[2]) + "\"" + col_str + "];\n"
+        )
+
+    output_file.write("}")
+    output_file.close()
+
+    # Make PNG from graph
+    (output_graph,) = pydot.graph_from_dot_file(filename)
+    output_graph.write_png("output_graph.png")
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     dotfile = sys.argv[1]
@@ -63,6 +108,7 @@ if __name__ == '__main__':
 
         if int(analysis_type) == 0:
             edges_w_gens = generation_analysis(root_node)
+            write_dot(graph, edges_w_gens)
         else:
             print("Analysis type not supported")
 
