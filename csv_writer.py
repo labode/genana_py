8a9b2ab1275189802ac574bb5f6fcc6b6b006bbd
Jonas Labode <labode.jonas@mh-hannover.de>
2021-05-28 17:50:49 +0200

    csv writer added for future use

---
+++ csv_writer.py
@@ -0,0 +1,26 @@
+import networkx as nx
+
+
+def write(graph, target_file, labels):
+
+    # Initiate File
+    filename = str(target_file) + ".csv"
+    output_file = open(filename, "w")
+
+    # Write header
+    output_file.write("start; end;")
+    for label in labels:
+        output_file.write(' ' + label + ';')
+    output_file.write("\n")
+
+    # Write data
+    # Get all edges
+    edges = list(nx.edges(graph))
+    for edge in edges:
+        output_file.write(str(edge[0]) + ' ;' + edge[1] + ';')
+        # Write each labels content
+        for label in labels:
+            output_file.write(' ' + str(graph[edge[0]][edge[1]][0][label]) + ';')
+        output_file.write("\n")
+
+    output_file.close()
